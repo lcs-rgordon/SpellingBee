ca116971d2c1becc9595019c0856d7e8d5b94516
Russell Gordon <github@russellgordon.ca>
2022-02-16 09:18:09 -0500

    Tap on the item and the word will be spoken.

--- SpellingBee/Views/ContentView.swift
+++ SpellingBee/Views/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Russell Gordon on 2022-02-16.
 //
 
+import AVFoundation
 import SwiftUI
 
 struct ContentView: View {
@@ -20,7 +21,25 @@ struct ContentView: View {
             Image(currentItem.imageName)
                 .resizable()
                 .scaledToFit()
-
+                .onTapGesture {
+                    
+                    // Create the word to be spoken (an utterance) and set
+                    // characteristics of how the voice will sound
+                    let utterance = AVSpeechUtterance(string: currentItem.word)
+                    
+                    // See a list of available language codes and their corresponding
+                    // voice names and genders here:
+                    // https://www.ikiapps.com/tips/2015/12/30/setting-voice-for-tts-in-ios
+                    utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+                    
+                    // How fast the utterance will be spoken
+                    utterance.rate = 0.5
+                    
+                    // Synthesize (speak) the utterance
+                    let synthesizer = AVSpeechSynthesizer()
+                    synthesizer.speak(utterance)
+                    
+                }
             
         }
         
