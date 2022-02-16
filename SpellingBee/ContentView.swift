d2eeb4de7e953ca0a5b3193259afae5445044ec3
Russell Gordon <github@russellgordon.ca>
2022-02-16 08:55:54 -0500

    Initial Commit

---
+++ SpellingBee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpellingBee
+//
+//  Created by Russell Gordon on 2022-02-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
